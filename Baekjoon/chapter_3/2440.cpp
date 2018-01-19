5c72f1b1e8fe1c435e122c2db74c5b3a8b15f839
Ggoom94 <cbg1541@gmail.com>
2018-01-20 01:05:32 +0900

    별찍기 반대로2 수정

--- Baekjoon/chapter_3/2440.cpp
+++ Baekjoon/chapter_3/2440.cpp
@@ -4,8 +4,8 @@ int main(void)
 {
 	int N;
 	cin >> N;
-	for(int i = N; i >0; N--){
-		for(int j = 1; j <=N;j++){
+	for (int i = N; i >0; i--) {
+		for (int j = 1; j <= i; j++) {
 			cout << "*";
 		}
 		cout << "\n";
