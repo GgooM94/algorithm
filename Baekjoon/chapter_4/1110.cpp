d8d3299310f45470124d4af240551ffbda4b4624
Ggoom94 <cbg1541@gmail.com>
2018-01-20 22:32:17 +0900

    더하기 사이클

---
+++ Baekjoon/chapter_4/1110.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+	unsigned short N(0), count(0), result(0), temp(0);
+	bool TF(true);
+	cin >> N;
+	temp = N;
+
+	while (TF) {
+		result = N / 10 + N % 10;
+		N = N % 10 * 10 + result % 10;
+		count++;
+		if (temp == N)
+			TF = false;
+	}
+	cout << count;
+
+}
