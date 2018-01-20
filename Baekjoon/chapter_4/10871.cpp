b6c31d023a28feda39270e30b1034249618308df
GgooM <cbg1541@gmail.com>
2018-01-20 15:32:23 +0900

    X 보다 작은 수

---
+++ Baekjoon/chapter_4/10871.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(void) {
+	unsigned int N, X;
+	cin >> N >> X;
+	int *arr = new int[N];
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	for (int i = 0; i < N; i++) {
+		if (arr[i] < X)
+			cout << arr[i] << " ";
+	}
+	delete[] arr;
+}
+
