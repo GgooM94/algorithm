1e9cf6b52d65376d16e6368d4300f48a0de56149
GgooM <cbg1541@gmail.com>
2018-01-24 16:12:04 +0900

    OX퀴즈 주석

--- Baekjoon/chapter_6/8958.cpp
+++ Baekjoon/chapter_6/8958.cpp
@@ -9,22 +9,25 @@ int main(void) {
 	string *result = new string[size];
 	int *res = new int[size];
 
-
 	for (int i = 0; i < size; i++) {
 		int score(0);
 		cin >> inputstr;
 		result[i] = inputstr;
-		int len = result[i].length();
-		int *output = new int[len] {0, };
+		int len = result[i].length();	//for문에 코드중복이 많아 선언
+		int *output = new int[len] {0, };	//OX를 점수화 시키기 위해 선언
 
+											//입력한 OX값들을 숫자 0,1로 변환
 		for (int k = 0; k < len; k++) {
 			if (result[i][k] == 'O')
 			{
 				output[k] = 1;
 			}
 		}
+		//다름 포문에서 0번째 인수값을 비교하지 않으므로 선언
 		if (output[0] == 1)
 			++score;
+
+		//이전 값이 정답일경우 점수 가중치를 두기 위해서
 		for (int j = 1; j < len; j++) {
 			if (output[j - 1] > 0 && output[j] >0) {
 				output[j] = output[j - 1] + 1;
@@ -32,9 +35,10 @@ int main(void) {
 			score += output[j];
 		}
 		res[i] = score;
-		delete output;
+		delete[] output;	//동적할당 뒤엔 항상 delete!!!!!!!
 	}
 	for (int i = 0; i < size; i++) {
 		cout << res[i] << "\n";
 	}
+	delete[] result;
 }
