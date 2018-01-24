3d8aebc7e1521c610d22a4d463d0a4a357f16a49
GgooM <cbg1541@gmail.com>
2018-01-24 16:54:35 +0900

    평균 점수 수정

--- Baekjoon/chapter_6/10039.cpp
+++ Baekjoon/chapter_6/10039.cpp
@@ -3,13 +3,13 @@ using namespace std;
 #define size 5
 int main(void)
 {
-	int n[size] = {0,};
-	int hap(0);
-	for(int i =0;i<size; i++){
-		cin >> n[i];
-		if(n[i] <40)
-			n[i] = 40;
-		hap += n[i];
+	unsigned short hap(0), score(0);
+	for (int i = 0; i<size; i++) {
+		cin >> score;
+		if (score < 40)
+			hap += 40;
+		else
+			hap += score;
 	}
-	cout << hap/size;
+	cout << hap / size;
 }
