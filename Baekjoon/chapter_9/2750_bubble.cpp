d1d480a4a04f2f018b2226c7f2b746db7b344cd2
Ggoom94 <cbg1541@gmail.com>
2018-01-27 13:24:25 +0900

    BubbleSort

--- Baekjoon/chapter_9/2750_bubble.cpp
+++ Baekjoon/chapter_9/2750_bubble.cpp
@@ -1,10 +1,22 @@
 #include<iostream>
 using namespace std;
-int main(void){
-	int N;
-	int *arr = new int[N]{0,};
-	
+int main(void) {
+	int N(0), temp(0);
+	cin >> N;
+	int *arr = new int[N] {0, };
 
-	
-
+	for (int i = 0; i<N; i++) {
+		cin >> arr[i];
+	}
+	for (int j = N; j > 0; j--) {
+		for (int i = 1; i < j; i++) {
+			if (arr[i - 1] > arr[i])
+			{
+				temp = arr[i - 1];
+				arr[i - 1] = arr[i];
+				arr[i] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < N; i++)	cout << arr[i] << "\n";
 }
