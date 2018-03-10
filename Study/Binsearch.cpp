13beaceac3dbc0518abf7173909307aa7a797f64
Ggoom94 <cbg1541@gmail.com>
2018-03-10 15:33:54 +0900

    이진 탐색

---
+++ Study/Binsearch.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+int binsearch(const std::vector<int> arr, int x) {
+	int n = arr.size();
+	int lo =0, hi = n;
+	while (lo + 1 < hi) {
+		int mid = (lo + hi) / 2;
+		if (arr[mid] < x)
+			lo = mid;
+		else
+			hi = mid;
+	}
+	return hi;
+}
+
+int main(void) {
+	using std::cout;
+	using std::cin;
+	int x;
+	std::vector<int> arr = { 1,2,3,4,5,6,7,8,9,10 };
+	cin >> x;
+	cout << binsearch(arr,x);
+}
