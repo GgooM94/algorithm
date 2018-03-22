1c175fe9b621ea631de0ab43c00d1a23f1ad5c56
GgooM <cbg1541@gmail.com>
2018-03-22 14:48:58 +0900

    플러그

---
+++ Baekjoon/2010.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int result(1),N,input;
+	cin >> N;
+	while (N--) {
+		cin >> input;
+		result += input - 1;
+	}
+	cout << result;
+
+}
