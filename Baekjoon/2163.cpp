549cc260d1f303afce319fb5e6cab7970afa0679
GgooM <cbg1541@gmail.com>
2018-03-22 14:36:23 +0900

    초콜릿 자르기

---
+++ Baekjoon/2163.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+int cnt(0);
+int count(int n) {
+	if (n == 1)
+		return 0;
+	else if (n == 2)
+		return 1;
+	return 1 +count(n / 2) + (count(n - n / 2));
+}
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int N, M;
+	cin >> N >> M;
+	cout << count(N*M);
+}
