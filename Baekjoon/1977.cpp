7e8f507645a8d9a04a8f92cc5801800de074b7dc
Ggoom94 <cbg1541@gmail.com>
2018-03-26 23:55:30 +0900

    완전제곱수

---
+++ Baekjoon/1977.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie();
+	bool arr[10001] = { 0, };
+	int i, M, N;
+	int tot(0),minRes =987654321;
+	for (i = 1; i < 101; i++) {
+		arr[i*i] = 1;
+	}
+	
+	cin >> M >> N;
+
+	for (i = M; i <= N; i++) {
+		if (arr[i] == 1) {
+			minRes = min(minRes, i);
+			tot += i;
+		}
+	}
+
+	if (tot == 0)
+		cout << -1;
+	else
+		cout << tot << "\n" << minRes;
+}
