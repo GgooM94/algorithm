159d8d7ecc6039602b2d8127015cabcea62016e0
Ggoom94 <cbg1541@gmail.com>
2018-04-15 21:10:10 +0900

    DP, 1차원 배열 DP 피보나치

---
+++ Baekjoon/chapter_15/11051_better.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#define MOD 10007
+int N, K, i, j;
+int DP[1001] = { 0 };
+
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> N >> K;
+	for (i = 1; i <= N; i++) {
+		DP[i] = 1;
+
+		for (j = i-1; j > 1; j--) {
+			DP[j] = (DP[j] + DP[j - 1])% MOD;
+		}
+		DP[j] = i;
+	}
+	cout << DP[K];
+}
