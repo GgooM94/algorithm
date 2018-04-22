4954efdd3c0a6349b3c4c9c1e91d03306b81acca
Ggoom94 <cbg1541@gmail.com>
2018-04-23 00:01:20 +0900

    닉네임에 갓 붙이기

---
+++ Baekjoon/10000/13163.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int N;
+string str;
+bool flag = false;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> N;
+	cin.ignore();
+	while (N--)
+	{
+		getline(cin, str);
+		cout << "god";
+		for (int i = 0; i < str.size(); i++) {
+			if (str[i] == ' ')
+				flag = true;
+			if (flag && str[i] != ' ') {
+				cout << str[i];
+			}
+		}
+		cout << "\n";
+		flag = false;
+	}
+}
