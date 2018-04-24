fb806b314d082c19aa1e9584da9aa2681f135aca
Ggoom94 <cbg1541@gmail.com>
2018-04-24 23:29:55 +0900

    문자열 거꾸로 출력

---
+++ Baekjoon/10000/11365.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	string str;
+	getline(cin, str);
+	while (str != "END")
+	{
+		for (int i = str.size() - 1; i >= 0; i--) {
+			cout << str[i];
+		}
+		cout << "\n";
+		getline(cin, str);
+	}
+}
