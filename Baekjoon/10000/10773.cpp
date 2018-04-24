0897c5bcb6bdb4e2a737ee1106ebf5e5c4e00d59
Ggoom94 <cbg1541@gmail.com>
2018-04-24 23:39:36 +0900

    스택을 이용한 문제, 제로

---
+++ Baekjoon/10000/10773.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<vector>
+
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int K, N;
+	vector<int>input;
+	cin >> K;
+
+	for (int i = 0; i < K; i++) {
+		cin >> N;
+		if (N == 0)input.pop_back();
+		else input.push_back(N);
+	}
+
+	int cnt(0);
+	for (int i = 0; i < input.size(); i++) {
+		cnt += input[i];
+	}
+	cout << cnt;
+}
