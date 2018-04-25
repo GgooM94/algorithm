caeaf474d32e27697d164d1dc0b33b8ff1f865f1
Ggoom94 <cbg1541@gmail.com>
2018-04-26 02:15:59 +0900

    조세퍼스 문제0

---
+++ Baekjoon/11000/11866.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+using namespace std;
+
+bool visit[1001] = { 0 };
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N, M, res(0), cnt(0), i(0);
+	cin >> N >> M;
+
+	cout << "<";
+	while (true)
+	{
+		cnt = 0;
+		while (true)
+		{
+			i++;
+			if (i == N + 1)i = 1;
+			if (visit[i] == 1) continue;
+			cnt++;
+			if (visit[i] == 0 && cnt == M) {
+				cout << i;
+				visit[i] = 1;
+				res++;
+				break;
+			}
+		}
+		if (res == N)
+			break;
+		cout << ", ";
+	}
+	cout << ">";
+}
