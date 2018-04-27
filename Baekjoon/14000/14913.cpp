37241d6d817059be86778167c4c5cd8cfb9bab82
Ggoom94 <cbg1541@gmail.com>
2018-04-28 00:41:24 +0900

    등차수열에서 항 번호 찾기

---
+++ Baekjoon/14000/14913.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int a, b, k;
+	cin >> a >> b >> k;
+
+	if ((k - a) % b != 0 || (k - a) / b <0) {
+		cout << "X";
+	}
+	else {
+		cout << abs((k - a) / b) + 1;
+	}
+}
