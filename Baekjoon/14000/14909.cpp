10b147be624447eb73f85301d30f61857fb2fc7b
Ggoom94 <cbg1541@gmail.com>
2018-04-30 01:29:27 +0900

    양수 개수 세기

---
+++ Baekjoon/14000/14909.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int input, tot(0);
+
+	while (cin >> input)
+	{
+		if (input > 0)
+			tot++;
+	}
+	cout << tot;
+}
