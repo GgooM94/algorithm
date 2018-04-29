f45373ac1095afe2a5b38ded955bfa698a10d6e9
Ggoom94 <cbg1541@gmail.com>
2018-04-30 01:37:38 +0900

    오르막

---
+++ Baekjoon/14000/14910.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int input, tmp(-987654321);
+
+	bool flag = false;
+	while (cin >> input)
+	{
+		if (tmp > input) {
+			flag = true;
+		}
+		tmp = input;
+	}
+	flag ? (cout << "Bad") : (cout << "Good");
+}
