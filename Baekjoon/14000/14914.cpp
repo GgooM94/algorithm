91d8eeb90702fd0c8f3693da426ee0d2d467d4f1
Ggoom94 <cbg1541@gmail.com>
2018-04-30 01:26:37 +0900

    사과와 바나나 나눠주기

---
+++ Baekjoon/14000/14914.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int a, b;
+	cin >> a >> b;
+
+	for (int i = 1; i <= a; i++) {
+		if (a%i == 0 && b%i == 0) {
+			cout << i << " " << a / i << " " << b / i << "\n";
+		}
+	}
+}
