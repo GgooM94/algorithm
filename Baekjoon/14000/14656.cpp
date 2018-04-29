9367eccf0f637798944060c93c0cb82bfe5fcac9
Ggoom94 <cbg1541@gmail.com>
2018-04-30 01:21:08 +0900

    조교는 새디스트야git add 14656.cpp

---
+++ Baekjoon/14000/14656.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N, tot(0);
+	cin >> N;
+	vector<int>arr;
+	arr.resize(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+		if (arr[i] != i + 1)
+			tot++;
+	}
+	cout << tot;
+}
