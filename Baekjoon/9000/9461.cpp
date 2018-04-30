425ab1ee8e8b8c84e88129998c34735ed90ed270
Ggoom94 <cbg1541@gmail.com>
2018-05-01 00:01:00 +0900

    파도반 수열

---
+++ Baekjoon/9000/9461.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+typedef long long ll;
+ll DP[101] = { 0,1,1,1,2 };
+
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int T, input;
+	cin >> T;
+	for (int i = 5; i < 101; i++) {
+		DP[i] = DP[i - 1] + DP[i - 5];
+	}
+
+	while (T--)
+	{
+		cin >> input;
+		cout << DP[input] << "\n";
+	}
+}
