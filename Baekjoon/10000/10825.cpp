b913b235581ebb89972ab37887590101b4a643a1
Ggoom94 <cbg1541@gmail.com>
2018-05-05 20:36:34 +0900

    국영수

---
+++ Baekjoon/10000/10825.cpp
@@ -0,0 +1,50 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+struct student
+{
+	string name;
+	int kor;
+	int eng;
+	int mat;
+};
+
+bool comp(const student &a, const student& b) {
+	if (a.kor == b.kor) {
+		if (a.eng != b.eng) {
+			return a.eng < b.eng;
+		}
+		else {
+			if (a.mat != b.mat) {
+				return a.mat > b.mat;
+			}
+			else {
+				return a.name < b.name;
+			}
+		}
+	}
+	return a.kor > b.kor;
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	vector<student> arr(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i].name >> arr[i].kor >> arr[i].eng >> arr[i].mat;
+	}
+	sort(arr.begin(), arr.end(), comp);
+
+	for (int i = 0; i < N; i++) {
+		cout << arr[i].name << "\n";
+	}
+}
