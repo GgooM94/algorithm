b157e4a6415f2bc3820d20d072328963a5483e95
Ggoom94 <cbg1541@gmail.com>
2018-05-08 00:24:23 +0900

    소인수분해

---
+++ Baekjoon/11000/11653.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+
+void calc(int n) {
+	for (int i = 2; i <= n; i++) {
+		if (n % i == 0) {
+			std::cout << i << "\n";
+			calc(n / i);
+			break;
+		}
+	}
+}
+
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+	calc(N);
+}
