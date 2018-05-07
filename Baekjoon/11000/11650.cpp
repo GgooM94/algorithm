618f929312087fc940f02b6aa3af8df7ef11bbe9
Ggoom94 <cbg1541@gmail.com>
2018-05-07 20:11:04 +0900

    좌표 정렬하기

---
+++ Baekjoon/11000/11650.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+
+struct points
+{
+	int x;
+	int y;
+};
+
+bool comp(const points &a, const points &b) {
+	if (a.x == b.x) {
+		return a.y < b.y;
+	}
+	return a.x < b.x;
+}
+
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+	vector<points> input(N);
+	for (int i = 0; i < N; i++) {
+		cin >> input[i].x >> input[i].y;
+	}
+	sort(input.begin(), input.end(), comp);
+
+	for (int i = 0; i < N; i++)
+		cout << input[i].x << " " << input[i].y << "\n";
+}
