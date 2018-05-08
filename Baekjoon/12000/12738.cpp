e329a16a12b45d11f747484fd8205475506f0e9f
Ggoom94 <cbg1541@gmail.com>
2018-05-09 02:02:34 +0900

    LIS, 가장 긴 증가하는 부분 수열3

---
+++ Baekjoon/12000/12738.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+
+typedef long long ll;
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+	vector<ll>arr(N);
+	vector<ll>LIS;
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	LIS.push_back(arr[0]);
+
+	for (int i = 1; i < N; i++) {
+		if (LIS.back() < arr[i]) {
+			LIS.push_back(arr[i]);
+		}
+		else {
+			*lower_bound(LIS.begin(), LIS.end(), arr[i]) = arr[i];
+		}
+	}
+	cout << LIS.size();
+}
