a080e736694325064dbd1078248b9408e22b9751
GgooM <cbg1541@gmail.com>
2018-05-08 15:50:48 +0900

    DFS, N과 M(4)

---
+++ Baekjoon/15000/15651.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+
+int N, M;
+int arr[10] = { 0 };
+void DFS(int n) {
+	if (n == M) {
+		for (int i = 0; i < M; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = 0; i < N; i++) {
+		arr[n] = i + 1;
+		DFS(n + 1);
+	}
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> N >> M;
+	DFS(0);
+}
