a21f3419960ddfae93c441efc9fe58a230973192
Ggoom94 <cbg1541@gmail.com>
2018-05-09 02:12:08 +0900

    2018 연세대학교 프로그래밍 경진대회

---
+++ Baekjoon/15000/15667.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N, i(1);
+	cin >> N;
+
+	while (true)
+	{
+		if (i *(i + 1) == N - 1)
+			break;
+		i++;
+	}
+	cout << i;
+}
