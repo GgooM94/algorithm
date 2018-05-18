3f35860a7206ed29b09e6d698c65bf9dc2f516d7
Ggoom94 <cbg1541@gmail.com>
2018-05-19 00:33:26 +0900

    붕어빵 판매하기

---
+++ Baekjoon/11000/11052.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+	vector<int>input(N + 1);
+	vector<int>DP(N + 1, 0);
+
+	for (int i = 1; i <= N; i++)
+		cin >> input[i];
+
+	DP[1] = input[1];
+
+	for (int i = 1; i < input.size(); i++) {
+		for (int j = 1; j <= i; j++) {
+			if (i - j >= 0)
+				DP[i] = max(DP[i], DP[i - j] + input[j]);
+		}
+	}
+	cout << DP[N];
+}
