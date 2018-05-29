150f11317ea55595687bbe41bbdc4a7d83ef54eb
Ggoom94 <cbg1541@gmail.com>
2018-05-29 22:46:45 +0900

    행렬 대각선 합의 차, 절대값

---
+++ Hackerrank/DiagonalDifference.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the diagonalDifference function below.
+int diagonalDifference(vector<vector<int>> a) {
+
+	int index = a.size();
+	int left(0), right(0);
+	for (int i = 0; i<index; i++) {
+		left += a[i][i];
+	}
+
+	for (int i = 0; i<index; i++) {
+		right += a[i][index - i - 1];
+	}
+	return abs(left - right);
+}
+
+int main()
+{
+	ofstream fout(getenv("OUTPUT_PATH"));
+
+	int n;
+	cin >> n;
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+	vector<vector<int>> a(n);
+	for (int i = 0; i < n; i++) {
+		a[i].resize(n);
+
+		for (int j = 0; j < n; j++) {
+			cin >> a[i][j];
+		}
+
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	}
+
+	int result = diagonalDifference(a);
+
+	fout << result << "\n";
+
+	fout.close();
+
+	return 0;
+}
