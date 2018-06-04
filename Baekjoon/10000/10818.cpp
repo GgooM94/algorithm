41a5e1da331cce225816573cee64e10dd17a8348
Ggoom94 <cbg1541@gmail.com>
2018-06-04 12:17:33 +0900

    최소, 최대

---
+++ Baekjoon/10000/10818.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<algorithm>
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N, minN(987654321), maxN(-987654321);
+	int temp;
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> temp;
+		minN = min(minN, temp);
+		maxN = max(maxN, temp);
+	}
+	cout << minN << ' ' << maxN;
+}
