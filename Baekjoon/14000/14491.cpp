8dac5b0be5c57b48d35afd15b25f7c538fd0a812
Ggoom94 <cbg1541@gmail.com>
2018-06-09 18:56:31 +0900

    9진수

---
+++ Baekjoon/14000/14491.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include<string>
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int T;
+	string res = "";
+	cin >> T;
+	while (T)
+	{
+		res += T % 9 + '0';
+		T /= 9;
+	}
+
+	for (int i = res.size() - 1; i >= 0; i--)
+		cout << res[i];
+}
