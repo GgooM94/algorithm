aebdabcdf573e0d9a2df44ec210d7d81fb81c918
Ggoom94 <cbg1541@gmail.com>
2018-06-15 23:25:18 +0900

    치킨 두마리(...)

---
+++ Baekjoon/14000/14489.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	long long A, B, C;
+	cin >> A >> B >> C;
+
+	if ((A + B) >= 2 * C) {
+		cout << A + B - (2 * C);
+	}
+	else {
+		cout << A + B;
+	}
+
+}
