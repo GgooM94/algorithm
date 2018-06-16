ec3ffce396aa69ec63c5b9ecf1ab0b29f1cf71e4
Ggoom94 <cbg1541@gmail.com>
2018-06-16 12:14:24 +0900

    욱제는 효도쟁이야git add 14487.cpp

---
+++ Baekjoon/14000/14487.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<algorithm>
+
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N, hap(0), maxN(-1);
+	cin >> N;
+
+	int *arr = new int[N];
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+		hap += arr[i];
+		maxN = max(maxN, arr[i]);
+	}
+
+	cout << hap - maxN;
+	delete[] arr;
+}
