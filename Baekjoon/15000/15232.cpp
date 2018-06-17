97a0d4eac554bd932831ca12ce006c41d714a8e5
Ggoom94 <cbg1541@gmail.com>
2018-06-17 17:15:37 +0900

    Rectangles

---
+++ Baekjoon/15000/15232.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N, M;
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++)
+			cout << '*';
+		cout << '\n';
+	}
+}
