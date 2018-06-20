131d96c83362bc25cd020af164fb988721a932bd
Ggoom94 <cbg1541@gmail.com>
2018-06-20 20:49:56 +0900

    백대열

---
+++ Baekjoon/14000/14490.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<string>
+#include<algorithm>
+
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	string str;
+	cin >> str;
+
+	int index(0);
+
+	for (int i = 0; i < str.size(); i++) {
+		if (str[i] == ':') {
+			index = i;
+			break;
+		}
+	}
+
+	int A(0), B(0);
+	A = stoi(str.substr(0, index));
+	B = stoi(str.substr(index + 1));
+
+	while (B>0)
+	{
+		int temp = B;
+		B = A%B;
+		A = temp;
+	}
+	cout << stoi(str.substr(0, index)) / A << ':' << stoi(str.substr(index + 1)) / A;
+}
