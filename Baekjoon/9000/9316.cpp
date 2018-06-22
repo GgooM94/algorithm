8c4f2cce9ba9a05ee4b62fbb609c8236a7528915
Ggoom94 <cbg1541@gmail.com>
2018-06-22 14:00:55 +0900

    Hello Judge

---
+++ Baekjoon/9000/9316.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+
+int main(void) {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		cout << "Hello World, Judge " << i << "!\n";
+	}
+}
