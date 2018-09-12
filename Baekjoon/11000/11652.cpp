821da85331ef3abe4f8942a31cfec9792022a663
Ggoom94 <cbg1541@gmail.com>
2018-09-12 18:51:44 +0900

    카드 정렬

---
+++ Baekjoon/11000/11652.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+struct Point
+{
+	int x, y;
+};
+
+bool cmp(const Point& a, const Point& b)
+{
+	if (a.y < b.y)
+		return true;
+	if (a.y == b.y && a.x < b.x)
+		return true;
+
+	return false;
+}
+int main(void)
+{
+	int N;
+	cin >> N;
+
+	vector<Point>arr(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i].x >> arr[i].y;
+	}
+	sort(arr.begin(), arr.end(), cmp);
+
+	for (int i = 0; i < N; i++) {
+		cout << arr[i].x << ' ' << arr[i].y << '\n';
+	}
+}
