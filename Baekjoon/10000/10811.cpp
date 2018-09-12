fdda50da75e9c2c2beb1a84bc7103efa29018d9b
Ggoom94 <cbg1541@gmail.com>
2018-09-12 17:07:38 +0900

    바구니 뒤집기

---
+++ Baekjoon/10000/10811.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+void reverseArr(vector<int>&arr, int left, int right)
+{
+	int temp;
+
+	while (left < right)
+	{
+		temp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = temp;
+
+		left++;
+		right--;
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N, M;
+	cin >> N >> M;
+
+	vector<int>arr(N);
+
+	for (int i = 0; i < N; i++)
+		arr[i] = i + 1;
+
+	int left, right;
+	for (int i = 0; i < M; i++)
+	{
+		cin >> left >> right;
+		reverseArr(arr, left - 1, right - 1);
+	}
+
+	for (int i = 0; i < N; i++)
+		cout << arr[i] << ' ';
+}
