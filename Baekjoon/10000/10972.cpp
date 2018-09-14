063eec765c6ca8881899dc28962189af5bc913a0
Ggoom94 <cbg1541@gmail.com>
2018-09-14 23:11:01 +0900

    다음 순열

---
+++ Baekjoon/10000/10972.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main(void) {
+	int N;
+	cin >> N;
+	vector<int>arr(N);
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+	if (next_permutation(arr.begin(), arr.end()))
+	{
+		for (int i = 0; i < N; i++)
+			cout << arr[i] << ' ';
+	}
+	else {
+		cout << -1;
+	}
+}
