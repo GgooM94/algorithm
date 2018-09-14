03e076712c3fb9c0138a33905122074b08454db4
Ggoom94 <cbg1541@gmail.com>
2018-09-14 23:55:44 +0900

    차이를 최대로

---
+++ Baekjoon/10000/10819.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main(void) {
+	int N;
+	cin >> N;
+	vector<int>arr(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	sort(arr.begin(), arr.end());
+
+	int res = -1;
+	do {
+		int sum(0);
+		for (int i = 1; i < arr.size(); i++) {
+			sum += abs(arr[i - 1] - arr[i]);
+		}
+		res = max(res, sum);
+
+	} while (next_permutation(arr.begin(), arr.end()));
+
+	cout << res;
+}
---
+++ Baekjoon/10000/10974.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(void) {
+	int N;
+	cin >> N;
+	vector<int>arr(N);
+
+	for (int i = 0; i < N; i++)
+		arr[i] = i + 1;
+
+	while (true)
+	{
+		for (int i = 0; i < N; i++)
+			cout << arr[i] << ' ';
+		cout << '\n';
+
+		if (!next_permutation(arr.begin(), arr.end()))
+			break;
+	}
+}
