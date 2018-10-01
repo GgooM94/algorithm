eece9081e22f3d8cec93d83f4a0f4b7c262d9634
GgooM <cbg1541@gmail.com>
2018-10-01 19:46:40 +0900

    Kangaroo

--- Hackerrank/Kangaroo.cpp
+++ Hackerrank/Kangaroo.cpp
@@ -9,10 +9,14 @@ vector<string> split_string(string);
 
 // Complete the kangaroo function below.
 string kangaroo(int x1, int v1, int x2, int v2) {
-	int temp = (x1 - x2) / (v2 - v1);
+	float temp;
+	if (v2 - v1 != 0 && x1 - x2 != 0) {
+		temp = (x1 - x2) / (v2 - v1);
+	}
 	if (temp < 0)
 		return "NO";
-	if (x1 - x2 == (v2 - v1) + temp) {
+
+	if (x1 - x2 == (v2 - v1) * temp) {
 		return "YES";
 	}
 	else {
