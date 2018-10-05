5df9b12bf09c8aeebc1eefcbb018e2b3187b2e69
Ggoom94 <cbg1541@gmail.com>
2018-10-05 22:34:56 +0900

    Circular_Array_Rotation

--- Hackerrank/Easy/Circular_Array_Rotation.cpp
+++ Hackerrank/Easy/Circular_Array_Rotation.cpp
@@ -1,38 +1,102 @@
 #include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
 
-int map[10][10] = { 0 , };
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
+using namespace std;
 
-int main(void) {
-	using namespace std;
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+vector<string> split_string(string);
 
-	int X, Y;
-	int	dir = 0;
-	cin >> X >> Y;
+// Complete the circularArrayRotation function below.
+vector<int> circularArrayRotation(vector<int> a, int k, vector<int> queries) {
+	vector<int>res;
+	k %= a.size();
+	int loc = a.size() - k;
+	reverse(a.begin(), a.begin() + loc);
+	reverse(a.begin() + loc, a.end());
+	reverse(a.begin(), a.end());
 
-	for (int i = 1; i <= Y; i++) {
-		for (int j = 1; j <= X; j++) {
-			map[i][j] = -1;
+	for (int i = 0; i < queries.size(); i++) {
+		res.push_back(a[queries[i]]);
+	}
+	return res;
+}
+
+int main()
+{
+	string nkq_temp;
+	getline(cin, nkq_temp);
+
+	vector<string> nkq = split_string(nkq_temp);
+
+	int n = stoi(nkq[0]);
+
+	int k = stoi(nkq[1]);
+
+	int q = stoi(nkq[2]);
+
+	string a_temp_temp;
+	getline(cin, a_temp_temp);
+
+	vector<string> a_temp = split_string(a_temp_temp);
+
+	vector<int> a(n);
+
+	for (int i = 0; i < n; i++) {
+		int a_item = stoi(a_temp[i]);
+
+		a[i] = a_item;
+	}
+
+	vector<int> queries(q);
+
+	for (int i = 0; i < q; i++) {
+		int queries_item;
+		cin >> queries_item;
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+		queries[i] = queries_item;
+	}
+
+	vector<int> result = circularArrayRotation(a, k, queries);
+
+	for (int i = 0; i < result.size(); i++) {
+		cout << result[i];
+
+		if (i != result.size() - 1) {
+			cout << "\n";
 		}
 	}
-	int x = 1;
-	int y = 1;
-	for (int i = 0; i < X*Y; i++) {
-		map[y][x] = i;
-		if (map[y + dy[dir]][x + dx[dir]] != -1) {
-			dir++;
-			dir %= 4;
-		}
-		y += dy[dir];
-		x += dx[dir];
+
+	cout << "\n";
+	return 0;
+}
+
+vector<string> split_string(string input_string) {
+	string::iterator new_end = unique(input_string.begin(), input_string.end(), [](const char &x, const char &y) {
+		return x == y && x == ' ';
+	});
+
+	input_string.erase(new_end, input_string.end());
+
+	while (input_string[input_string.length() - 1] == ' ') {
+		input_string.pop_back();
 	}
-	for (int i = 1; i <= Y; i++) {
-		for (int j = 1; j <= X; j++) {
-			cout << map[i][j] << '\t';
-		}
-		cout << endl;
+
+	vector<string> splits;
+	char delimiter = ' ';
+
+	size_t i = 0;
+	size_t pos = input_string.find(delimiter);
+
+	while (pos != string::npos) {
+		splits.push_back(input_string.substr(i, pos - i));
+
+		i = pos + 1;
+		pos = input_string.find(delimiter, i);
 	}
+
+	splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+	return splits;
 }
